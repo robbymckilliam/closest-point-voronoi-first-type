160827907ce7ae0445c64549f97d8a3cbcd4f5f8
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-03 22:37:40 +0930

    added plot.  Need to fill in code and generate data

--- findsuperbase/code/montecarlo.scala
+++ findsuperbase/code/montecarlo.scala
@@ -9,16 +9,18 @@ val Ns = List(1,2,3,4,5)
 
 val starttime = (new java.util.Date).getTime
 
-Ns.map{ n =>
+val datalist = Ns.map{ n =>
+val p = 1.0
+(n,p,0.1)
+}.toList
 
+val file = new java.io.FileWriter("data/testname")
+file.write("n" + "\t" + "proportion" + "\t" + "stddev" + "\n")
+datalist.foreach{ v => 
+  val (n,p,d) = v
+  file.write(n + "\t" + p + "\t" + d + "\n")
 }
-
-
+file.close
 
 val runtime = (new java.util.Date).getTime - starttime
 println("Finished in " + (runtime/1000.0) + " seconds.\n") 
-
-
-
-
-
