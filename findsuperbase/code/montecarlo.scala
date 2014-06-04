8a3ee4c3eba3a1c2a742f3f5fa0653d2b4e6ba4f
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-04 11:26:09 +0930

    added java libs

--- findsuperbase/code/montecarlo.scala
+++ findsuperbase/code/montecarlo.scala
@@ -1,6 +1,6 @@
 /**
-* Run montecarlo.
-*/
+  * Run montecarlo.
+  */
 import pubsim.distributions.RealRandomVariable
 import pubsim.distributions.UniformNoise
 import pubsim.distributions.GaussianNoise
@@ -13,17 +13,23 @@ val Ns = List(1,2,3,4,5)
 val starttime = (new java.util.Date).getTime
 
 val datalist = Ns.map{ n =>
-val p = 1.0
-(n,p,0.1)
+
+  //run all the MonteCarlo iterations
+  iters.map { i =>
+    val B = randomMatrix(n,n,UniformNoise.constructFromMinMax(0,1))
+  }.toList
+
+  val p = 1.0
+  (n,p,0.1)
 }.toList
 
 val file = new java.io.FileWriter("data/testname")
 file.write("n" + "\t" + "proportion" + "\t" + "stddev" + "\n")
-datalist.foreach{ v => 
+datalist.foreach{ v =>
   val (n,p,d) = v
   file.write(n + "\t" + p + "\t" + d + "\n")
 }
 file.close
 
 val runtime = (new java.util.Date).getTime - starttime
-println("Finished in " + (runtime/1000.0) + " seconds.\n") 
+println("Finished in " + (runtime/1000.0) + " seconds.\n")
