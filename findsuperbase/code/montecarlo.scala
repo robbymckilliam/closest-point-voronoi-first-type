f5d421d36d3631e27fb852f781cbd0ed18387466
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-04 15:32:12 +0930

    added lib and data

--- findsuperbase/code/montecarlo.scala
+++ findsuperbase/code/montecarlo.scala
@@ -6,30 +6,49 @@ import pubsim.distributions.UniformNoise
 import pubsim.distributions.GaussianNoise
 import pubsim.lattices.firstkind.FirstKindCheck
 import pubsim.VectorFunctions.randomMatrix
+import pubsim.VectorFunctions.randomMIMObasis
+import Jama.Matrix
+import scala.math.sqrt
 
-val iters = 10
-val Ns = List(1,2,3,4,5)
+val z = 1.96 //corresponds with 90% confidence intervals
+val L = 500 //the number of Monte Carlo iterations
+val Ns = List(1,2,3,4)
 
-val starttime = (new java.util.Date).getTime
+runsim( n=>randomMatrix(n,n,UniformNoise.constructFromMinMax(0,1)), "uniform")
+runsim( n=>randomMatrix(n,n,new GaussianNoise(0,1)), "gaussian")
+runsim( n=>randomMIMObasis(n), "mimo")
 
-val datalist = Ns.map{ n =>
+def runsim( basisgen : Int => Matrix, name : String ) {
 
-  //run all the MonteCarlo iterations
-  iters.map { i =>
-    val B = randomMatrix(n,n,UniformNoise.constructFromMinMax(0,1))
+  val starttime = (new java.util.Date).getTime
+
+  print(name + " ")
+
+  val datalist = Ns.map{ n =>
+
+    //run all the MonteCarlo iterations
+    val bdata = (1 to L).map { i =>
+      val B = basisgen(n)
+      val fk = new FirstKindCheck(B)
+      if(fk.isFirstKind) 1 else 0
+    }.toList
+
+    val phat = bdata.foldLeft(0.0) { (sum, v) => sum + v } / L
+    val ci = z * sqrt(phat*(1-phat)/ L )
+
+    print(".")
+    (n,phat,ci)
   }.toList
 
-  val p = 1.0
-  (n,p,0.1)
-}.toList
+  val file = new java.io.FileWriter("data/" + name)
+  file.write("n" + "\t" + "proportion" + "\t" + "stddev" + "\n")
+  datalist.foreach{ v =>
+    val (n,p,d) = v
+    file.write(n + "\t" + p + "\t" + d + "\n")
+  }
+  file.close
 
-val file = new java.io.FileWriter("data/testname")
-file.write("n" + "\t" + "proportion" + "\t" + "stddev" + "\n")
-datalist.foreach{ v =>
-  val (n,p,d) = v
-  file.write(n + "\t" + p + "\t" + d + "\n")
+  val runtime = (new java.util.Date).getTime - starttime
+  println(" finished in " + (runtime/1000.0) + " seconds.\n")
+
 }
-file.close
-
-val runtime = (new java.util.Date).getTime - starttime
-println("Finished in " + (runtime/1000.0) + " seconds.\n")
