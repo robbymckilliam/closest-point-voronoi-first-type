28573ffacead13032d2e614e2dd537999d580303
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-07 10:35:02 +0930

    added function for generating mimo matrices

--- findsuperbase/code/montecarlo.m
+++ findsuperbase/code/montecarlo.m
@@ -2,11 +2,6 @@
 
 clear all;
 
-if( usejava('jvm') == 0 )
-    disp(['It appears you do not have a java virtual machine installed. ' ...
-          ' This is not going to work.']);
-end
-
 %add java libraries in lib folder to matlab
 javaclasspath('./lib/PubSim.jar');
 javaaddpath('./lib/JGraphT.jar');
@@ -17,16 +12,23 @@ javaaddpath('./lib/junit-4.11.jar');
 javaaddpath('./lib/RngPack.jar');
 javaaddpath('./lib/colt.jar');
 javaaddpath('./lib/jtransforms-2.4.jar');
-
 addpath('./matlab') %add the matlab functions we will use
 
-javaclasspath('-dynamic')
+%javaclasspath('-dynamic')
 
-z = 1.96; %corresponds with 95% confidence interval
-L = 10; %number of Monte-Carlo trials
-Ns = [1,2,3,4]; %the dimensions we will run simulations for
+L = 500; %number of Monte-Carlo trials
+Ns = [1,2,3,4,5]; %the dimensions we will run simulations for
 
 %start simulations for uniform, gaussian and MIMO matrices
-runmontecarlo( @(n) rand(n), Ns, L, z, 'uniform' ); 
-runmontecarlo( @(n) randn(n), Ns, L, z, 'gaussian' ); 
+disp('uniform');
+puniform = runmontecarlo( @(n) rand(n), Ns, L ); 
+disp('gaussian');
+pgaussian = runmontecarlo( @(n) randn(n), Ns, L ); 
+disp('mimo');
+pmimo = runmontecarlo( @(n) randomMIMOmatrix(n), Ns, L ); 
+
+
+plot(Ns, puniform, '.r'); hold on;
+plot(Ns, pgaussian, 'sb'); hold on;
+plot(Ns, pmimo, 'xg'); hold on;
 
